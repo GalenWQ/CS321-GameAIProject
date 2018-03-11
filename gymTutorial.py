98ecdcaa6cd381c78c156cbf50c95ff6cdbf3de8
GalenWQ <galen.wq@gmail.com>
2018-03-11 17:31:45 -0500

    actually kind of working but maybe not learning

--- gymTutorial.py
+++ gymTutorial.py
@@ -28,8 +28,8 @@ class DQNAgent:
         model = Sequential()
         # activation(dot(input, kernel) + bias) Three fully connected layers used here
         # this is a pretty sweet deal for this to be reduced to just this
-        model.add(Conv2D(128, (3, 3), input_shape=self.state_dimensions, activation='relu'))
-        model.add(Conv2D(128, (3, 3), activation='relu'))
+        model.add(Conv2D(128, 3, 3, input_shape=self.state_dimensions, activation='relu'))
+        model.add(Conv2D(128, 3, 3, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
 
 
@@ -45,22 +45,21 @@ class DQNAgent:
         #this is the random choice of acts
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
-
         act_values = self.model.predict(state)
-        print("It reaches this point")
-        return np.argmax(act_values[0])  # returns action
+        return np.argmax(act_values[0][0][0])  # returns action
 
     def replay(self, batch_size):
         #This is where reward happens
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
-            print("state:", state)
-            print("action:", action)
-            print("reward:", reward)
-            print("next_state:", next_state)
-            print("done:", done)
+            #print("state:", state)
+            #print("action:", action)
+            #print("reward:", reward)
+            #print("next_state:", next_state)
+            #print("done:", done)
             # target reward
             target = reward
+
             if not done:
                 # This predicts the reward
                 target = (reward + self.gamma *
@@ -81,10 +80,9 @@ class DQNAgent:
 if __name__ == "__main__":
     env = gym.make('Breakout-v0')
     state_dimensions = env.observation_space.shape
-    print(env.action_space)
     action_size = env.action_space.n
     state = env.reset()
-    print("state:", state)
+    #print("state:", state)
     agent = DQNAgent(state_dimensions, action_size)
     # agent.load("./save/cartpole-dqn.h5")
     done = False
@@ -92,21 +90,31 @@ if __name__ == "__main__":
 
     for e in range(EPISODES):
         state = env.reset()
-        print("state:", state)
+        #print("state:", state)
         #state = np.reshape(state, [4, ])
+        state = np.expand_dims(state, axis=0)
         #state = [1, 2, 3, 4]
         while not done:
             #print("going")
             env.render()
             action = agent.act(state)
+            #print("action:", action)
             next_state, reward, done, _ = env.step(action)
             reward = reward if not done else -10
+            #print("new reward:", reward)
             #next_state = np.reshape(next_state, [1, 4])
+            next_state = np.expand_dims(next_state, axis=0)
             agent.remember(state, action, reward, next_state, done)
             state = next_state
             if done:
+                #env.reset()
                 print("episode: {}/{}, e: {:.2}"
                       .format(e, EPISODES, agent.epsilon))
                 break
         if len(agent.memory) > batch_size:
             agent.replay(10)
+
+        done = False
+
+        if e % 10 == 0:
+            agent.save("save.h5")
